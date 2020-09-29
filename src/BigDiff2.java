2699b92f4a86fd22370da78d992f4890c38d9ec4
Jan Gevaert <jan.gevaert@gmail.com>
2020-09-29 06:29:07 -0600

    Initial commit

---
+++ src/BigDiff2.java
@@ -0,0 +1,17 @@
+public class BigDiff2 {
+  public int bigDiff(int[] nums) {
+    int max = nums[0];
+    int min = nums[0];
+
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] > max) {
+        max = nums[i];
+      }
+      if (nums[i] <= min) {
+        min = nums[i];
+      }
+    }
+    return (max - min);
+  }
+}
